4a3ae12e6d803a894f7ba82d350ec5a5b030243d
Oli <147245351+what-does-that-do@users.noreply.github.com>
2024-06-28 10:03:02 +0000

    Working fourier

--- my_own_fourier.py
+++ my_own_fourier.py
@@ -7,30 +7,44 @@ f = wave.open("test.wav", "rb")
 print("Sample frequency = ", f.getframerate())
 print("Total number of frames = ", f.getnframes())
 print("Framerate = ", f.getframerate())
+print("This file has",f.getnchannels(),"channels!")
+sample_width = f.getsampwidth()
+print("Wizard 🧙‍♂️ says bit depth =", sample_width)
 
 sampleWidth = f.getframerate()
-frames = f.readframes(1000)
-
-pcm_samples = np.frombuffer(frames, dtype="<h")
-normalised_amplitides = np.int16((pcm_samples / pcm_samples.max()) * 32767)
+frames = f.readframes(6000)
 amplitudes = []
 
+# Trial and errors each frequency
 for frequency in range(20, 300):
+    print("Checking",frequency,"hz (max 300)", end='\r')
     xcords = 0
     ycords = 0
 
-    #for sample in normalised_amplitides:
-        
-    for i in range(len(normalised_amplitides)):
+    #for each frame in the wav file, test if it could be that frequency
+    for i in range(len(frames)):
         angle = (i / sampleWidth * frequency * np.pi * 2) # finding angle between the y and the x axis
-        xcords += np.cos(angle) * normalised_amplitides[i]
-        ycords += np.sin(angle) * normalised_amplitides[i]
-    
+        # find x and y co-ords, using sochatoa find the amplitude
+        xcords += np.cos(angle) * frames[i]
+        ycords += np.sin(angle) * frames[i]
+    # find the average amplitude
     averagex = xcords / sampleWidth
     averagey = ycords / sampleWidth
     averageAmplitude = (averagex * averagex) + (averagey * averagey)
-
+    # add this amplitude to our list, to be analysed later.
     amplitudes.append(averageAmplitude)
 
-plt.plot(amplitudes)
+# plot on a graph against frequencies.
+# x = frequency range multiplied by bit depth, because if there are 2 bits per sample frequencies are halved...
+# y = amplitude for that frequency. If the frequency is around 0, it's not in the wav file.
+plt.plot(range(20*sample_width, 300*sample_width, sample_width), amplitudes)
 plt.savefig("amps.jpg")
+
+found_frequencies = []
+for i in range(len(amplitudes)):
+    if amplitudes[i] > 50: # a rough place on the graph that usually is accurate
+        frequency = (20 * sample_width) + (i * sample_width)
+        found_frequencies.append(frequency)
+
+print("Found frequencies:", found_frequencies)
+print("\n🧙‍♂️ Finished!!")
