f23a3ee2d9a432716b5bffaadb09bc2e4f4ca842
Oli <147245351+what-does-that-do@users.noreply.github.com>
2024-06-28 08:03:12 +0000

    Plotting graphs

---
+++ real_python_example.py
@@ -0,0 +1,26 @@
+# https://realpython.com/python-scipy-fft/#the-discrete-cosine-and-sine-transforms
+from scipy.fft import fft, fftfreq
+import numpy as np
+from matplotlib import pyplot as plt
+
+SAMPLE_RATE = 44100  # Hertz
+DURATION = 5  # Seconds
+
+def generate_sine_wave(freq, sample_rate, duration):
+    x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
+    frequencies = x * freq
+    # 2pi because np.sin takes radians
+    y = np.sin((2 * np.pi) * frequencies)
+    return x, y
+
+# Generate a 2 hertz sine wave that lasts for 5 seconds
+x, y = generate_sine_wave(2, SAMPLE_RATE, DURATION)
+
+_, nice_tone = generate_sine_wave(400, SAMPLE_RATE, DURATION)
+_, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
+noise_tone = noise_tone * 0.3
+
+mixed_tone = nice_tone + noise_tone
+
+plt.plot(mixed_tone)
+plt.savefig("mixed_tone.jpg")
--- scipy_fft.py
+++ scipy_fft.py
@@ -1,6 +1,7 @@
-from scipy.fft import fft, ifft
+from scipy.fft import fft, fftfreq
 import wave
 import numpy as np
+from matplotlib import pyplot as plt
 
 f = wave.open("test.wav", "rb")
 
@@ -8,13 +9,23 @@ print("Sample frequency = ", f.getframerate())
 print("Total number of frames = ", f.getnframes())
 print("Framerate = ", f.getframerate())
 
-print("Getting 1/32 second's worth of frames (", int(f.getframerate()/32), ") frames...")
-
 frames = f.readframes(f.getframerate())
 pcm_samples = np.frombuffer(frames, dtype="<h")
-normalized_amplitudes = pcm_samples / (2 ** 15)
+normalised_amplitides = np.int16((pcm_samples / pcm_samples.max()) * 32767)
+
+plt.plot(normalised_amplitides[:1000])
+plt.savefig("norm_amplitudes.jpg")
 
 min_freq_range = 20.0
 max_freq_range = f.getframerate() / 2 # nyquist
 
-print(fft(normalized_amplitudes))
+N = f.getnframes()
+
+yf = fft(normalised_amplitides)
+xf = fftfreq(N, 1 / f.getframerate())
+
+plt.plot(np.abs(yf))
+plt.savefig("yf.jpg")
+
+plt.plot(np.abs(xf))
+plt.savefig("xf.jpg")
